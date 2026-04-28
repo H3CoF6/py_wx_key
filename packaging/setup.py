351c44af4dd3b0d21b92c94c842417357e88c47f
H3CoF6 <1707889225@qq.com>
2026-04-29 01:16:20 +0800

    update version to 2.0.1

--- packaging/setup.py
+++ packaging/setup.py
@@ -6,7 +6,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name='wx_key',
-    version='2.0.0',
+    version='2.0.1',
     description='WeChat Key Hook',
     packages=find_packages(),
     package_data={
